60b78b3a2b82df74cbcb9001c6b19f4d62b1a8a2
Illia Potsepaiev <Illia_Potsepaiev@epam.com>
2017-05-05 12:44:40 +0300

    added service api

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import { CarouselModule } from './src/app/index';
+import { CarouselService } from './src/app/services/carousel.service';
 import { ICarouselConfig, AnimationConfig } from './src/app/services/declarations';
 
 export {
   CarouselModule,
+  CarouselService,
   ICarouselConfig,
   AnimationConfig
 };

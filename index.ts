3e0ca433f78b29225a974f9fd2eff066242131a2
bonjurmrfirst <Illia_Potsepaiev@epam.com>
2017-05-19 15:59:53 +0300

    minor changes

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import { CarouselModule } from './src/app/index';
-import { CarouselService } from './src/app/services/carousel.service';
+import { CarouselService, WindowWidthService } from './src/app/services';
 import { ICarouselConfig, AnimationConfig } from './src/app/services/declarations';
 
 export {
   CarouselModule,
   CarouselService,
+  WindowWidthService,
   ICarouselConfig,
   AnimationConfig
 };

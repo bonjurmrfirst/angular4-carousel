1eec22ad86c9ed466f3ae561b5b7974cb9bdf432
Ryan Woodcox <rwoodcox@bizstream.com>
2017-08-28 16:14:54 -0400

    Implemented OnChanges to support re-initliazing the component when input properties change.

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnDestroy, ViewChild, forwardRef } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, OnDestroy, ViewChild, forwardRef } from '@angular/core';
 import 'rxjs/add/operator/takeWhile';
 
 import { CarouselService, ICarouselConfig, WindowWidthService } from '../../services';
@@ -12,7 +12,7 @@ import { CarouselHandlerDirective } from '../../directives';
   templateUrl: 'carousel.template.html',
   styleUrls: ['assets/carousel.styles.scss']
 })
-export class CarouselComponent implements OnInit, OnDestroy {
+export class CarouselComponent implements OnInit, OnChanges, OnDestroy  {
   @Input() private sources: string[];
   @Input() private config: ICarouselConfig;
 
@@ -30,6 +30,14 @@ export class CarouselComponent implements OnInit, OnDestroy {
   constructor(private carouselService: CarouselService, private windowWidthService: WindowWidthService) { }
 
   ngOnInit() {
+    this.initData();
+  }
+
+  ngOnChanges() {
+    this.initData();
+  }
+
+  public initData() {
     this.galleryLength = this.sources.length;
 
     const [showImmediate, ...showWhenLoad] = this.sources;
